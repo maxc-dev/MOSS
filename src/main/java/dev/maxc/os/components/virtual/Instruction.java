0e48bee83e74ed5ff19d0b419dd9a931155a7bb5
Max Carter <maxcartr@gmail.com>
2020-04-14 15:34:11 +0100

    Added a Stack data structure and an InstructionStack subclass

---
+++ src/main/java/dev/maxc/os/components/virtual/Instruction.java
@@ -0,0 +1,8 @@
+package dev.maxc.os.components.virtual;
+
+/**
+ * @author Max Carter
+ * @since 14/04/2020
+ */
+public class Instruction {
+}
---
+++ src/main/java/dev/maxc/os/structures/Stack.java
@@ -0,0 +1,35 @@
+package dev.maxc.os.structures;
+
+import java.util.ArrayList;
+
+/**
+ * @author Max Carter
+ * @since 14/04/2020
+ */
+public class Stack<T> {
+    protected ArrayList<T> stack = new ArrayList<>();
+    protected int pointer = -1;
+
+    /**
+     * Pushes an item to the stack
+     */
+    public void push(T item) {
+        stack.add(item);
+        pointer++;
+    }
+
+    /**
+     * Returns the top of the stack.
+     * @return Returns null if the stack is empty
+     */
+    public T pop() {
+        if (pointer >= 0) {
+            T item = stack.get(pointer);
+            stack.remove(item);
+            pointer--;
+            return item;
+        } else {
+            return null;
+        }
+    }
+}
