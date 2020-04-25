b781708135f5891f74bdd590caefbfc955396049
Max Carter <maxcartr@gmail.com>
2020-04-25 17:31:07 +0100

    Started developing a memory allocation indexer where each subclass will allocate different parts of ram for the same process using different allocation algorithms

---
+++ src/main/java/dev/maxc/os/components/memory/indexer/FirstFit.java
@@ -0,0 +1,15 @@
+package dev.maxc.os.components.memory.indexer;
+
+import dev.maxc.os.components.memory.GroupedMemoryAddress;
+
+/**
+ * @author Max Carter
+ * @since 25/04/2020
+ */
+public class FirstFit extends MemoryAllocationIndexer {
+    @Override
+    public GroupedMemoryAddress getIndexAddressSlot(int size) {
+        //TODO
+        return null;
+    }
+}
---
+++ src/main/java/dev/maxc/os/components/memory/indexer/MemoryAllocationIndexer.java
@@ -0,0 +1,11 @@
+package dev.maxc.os.components.memory.indexer;
+
+import dev.maxc.os.components.memory.GroupedMemoryAddress;
+
+/**
+ * @author Max Carter
+ * @since 25/04/2020
+ */
+public abstract class MemoryAllocationIndexer {
+    public abstract GroupedMemoryAddress getIndexAddressSlot(int size);
+}
