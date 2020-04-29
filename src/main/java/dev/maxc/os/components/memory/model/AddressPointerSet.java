4b28000dd3f10971042ef704e335e2da5bea059d
Max Carter <maxcartr@gmail.com>
2020-04-29 13:42:14 +0100

    Renamed GroupedMemoryAddress.java to AddressPointerSet.java

--- src/main/java/dev/maxc/os/components/memory/model/AddressPointerSet.java
+++ src/main/java/dev/maxc/os/components/memory/model/AddressPointerSet.java
@@ -7,13 +7,13 @@ import dev.maxc.os.io.log.Status;
  * @author Max Carter
  * @since 25/04/2020
  */
-public class GroupedMemoryAddress {
+public class AddressPointerSet {
     private final int startPointer;
     private final int endPointer;
 
-    public GroupedMemoryAddress(int startPointer, int endPointer) {
+    public AddressPointerSet(int startPointer, int endPointer) {
         if (endPointer < startPointer) {
-            Logger.log(Status.ERROR, this, "A grouped memory address pointer set was created whereby the end pointer is greater than the start pointer.");
+            Logger.log(Status.ERROR, this, "An address pointer set was created whereby the end pointer is greater than the start pointer.");
             throw new IllegalArgumentException("The end pointer [" + endPointer + "] must be greater than the start pointer [" + startPointer + "].");
         }
         this.startPointer = startPointer;
