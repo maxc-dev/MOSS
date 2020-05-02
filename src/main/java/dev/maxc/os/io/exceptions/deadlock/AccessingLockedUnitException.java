349674a5fac9f97cd05ad830c1621d59d42756cb
Max Carter <maxcartr@gmail.com>
2020-05-02 14:23:54 +0100

    Empty constructor for accessing a locked unit (batch instructions) and empty queue exception

--- src/main/java/dev/maxc/os/io/exceptions/deadlock/AccessingLockedUnitException.java
+++ src/main/java/dev/maxc/os/io/exceptions/deadlock/AccessingLockedUnitException.java
@@ -7,6 +7,10 @@ import dev.maxc.os.components.memory.model.MemoryAddress;
  * @since 24/04/2020
  */
 public class AccessingLockedUnitException extends Exception {
+    public AccessingLockedUnitException() {
+        super();
+    }
+
     public AccessingLockedUnitException(MemoryAddress memoryAddress) {
         super("Process attempted to access a unit which is locked at address: " + memoryAddress.toString());
     }
---
+++ src/main/java/dev/maxc/os/io/exceptions/structures/EmptyQueueException.java
@@ -0,0 +1,11 @@
+package dev.maxc.os.io.exceptions.structures;
+
+/**
+ * @author Max Carter
+ * @since 02/05/2020
+ */
+public class EmptyQueueException extends Exception {
+    public EmptyQueueException() {
+        super();
+    }
+}
