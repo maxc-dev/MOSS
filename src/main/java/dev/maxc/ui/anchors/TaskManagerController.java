01d968be0dabae2e04882a394f5fa7a40534efdb
Max Carter <maxcartr@gmail.com>
2020-05-17 16:58:46 +0100

    Added a line chart to display MMU read/write requests

--- src/main/java/dev/maxc/ui/anchors/TaskManagerController.java
+++ src/main/java/dev/maxc/ui/anchors/TaskManagerController.java
@@ -4,6 +4,9 @@ import javafx.application.Platform;
 import javafx.collections.ListChangeListener;
 import javafx.fxml.FXML;
 import javafx.fxml.Initializable;
+import javafx.scene.chart.LineChart;
+import javafx.scene.chart.NumberAxis;
+import javafx.scene.chart.XYChart;
 import javafx.scene.control.ListView;
 import javafx.scene.layout.AnchorPane;
 
@@ -21,6 +24,16 @@ public class TaskManagerController implements Initializable {
     private ListView<String> outputListView;
     @FXML
     private ListView<String> consoleListView;
+    @FXML
+    private LineChart<Integer, Integer> memoryUsageChart;
+    @FXML
+    private NumberAxis xAxisMemory;
+    @FXML
+    private NumberAxis yAxisMemory;
+
+    private XYChart.Series<Integer, Integer> writeRequests = new XYChart.Series<>();
+    private XYChart.Series<Integer, Integer> readRequests = new XYChart.Series<>();
+    private int counter = 0;
 
     @Override
     public void initialize(URL url, ResourceBundle resourceBundle) {
@@ -28,12 +41,23 @@ public class TaskManagerController implements Initializable {
         output("Output online");
         console("Console Online");
 
-        outputListView.getItems().addListener((ListChangeListener<String>) change -> {
-            Platform.runLater(() -> outputListView.scrollTo(outputListView.getItems().size() - 1));
-        });
-        consoleListView.getItems().addListener((ListChangeListener<String>) change -> {
-            Platform.runLater(() -> consoleListView.scrollTo(consoleListView.getItems().size() - 1));
-        });
+        outputListView.getItems().addListener((ListChangeListener<String>) change -> Platform.runLater(() -> outputListView.scrollTo(outputListView.getItems().size() - 1)));
+        consoleListView.getItems().addListener((ListChangeListener<String>) change -> Platform.runLater(() -> consoleListView.scrollTo(consoleListView.getItems().size() - 1)));
+
+        readRequests.setName("Memory Read Requests");
+        writeRequests.setName("Memory Write Requests");
+        memoryUsageChart.getData().add(readRequests);
+        memoryUsageChart.getData().add(writeRequests);
+    }
+
+    public void addMemoryWrite(int read, int write) {
+        counter++;
+        writeRequests.getData().add(new XYChart.Data<>(counter, write));
+        readRequests.getData().add(new XYChart.Data<>(counter, read));
+        if (counter >= 30) {
+            readRequests.getData().remove(0);
+            writeRequests.getData().remove(0);
+        }
     }
 
     public void output(String text) {
